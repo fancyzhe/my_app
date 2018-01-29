e4f96ef2c71f1a98a9a0ba96873d0ee7a94fb84c
fanz01 <fanz01@mingyuanyun.com>
2018-01-29 17:40:11 +0800

    ''

--- src/public/Manage.js
+++ src/public/Manage.js
@@ -4,11 +4,32 @@
  */
 
 import React from 'react'
+import {Drawer, MenuItem, RaisedButton} from "material-ui";
+import {MuiThemeProvider} from "material-ui/styles/index";
 
-class Manage extends React.Component{
-    render(){
-        return(
-            <div>11111</div>
+class Manage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider>
+                    <RaisedButton
+                        label="Toggle Drawer"
+                        onClick={this.handleToggle}
+                    />
+                    <Drawer open={this.state.open}>
+                        <MenuItem>Menu Item</MenuItem>
+                        <MenuItem>Menu Item 2</MenuItem>
+                    </Drawer>
+                </MuiThemeProvider>
+            </div>
         )
     }
 }
