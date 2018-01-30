1651f40228c9e3585294c9a9e75d83b1c57584d0
fanz01 <fanz01@mingyuanyun.com>
2018-01-30 17:04:16 +0800

    '修改table'

--- src/common/table.js
+++ src/common/table.js
@@ -3,13 +3,76 @@
  * @fanz
  */
 
+import _ from 'lodash'
 import React from 'react';
 import {Table} from 'material-ui/Table'
+import {TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from "material-ui/Table/index";
 
-class Table extends React.Component{
-    render(){
-        return(
+const tableData = [
+    {
+        name: 'John Smith',
+        status: 'Employed',
+    },
+    {
+        name: 'Randal White',
+        status: 'Unemployed',
+    },
+    {
+        name: 'Stephanie Sanders',
+        status: 'Employed',
+    },
+    {
+        name: 'Steve Brown',
+        status: 'Employed',
+    },
+    {
+        name: 'Joyce Whitten',
+        status: 'Employed',
+    },
+    {
+        name: 'Samuel Roberts',
+        status: 'Employed',
+    },
+    {
+        name: 'Adam Moore',
+        status: 'Employed',
+    },
+];
+
+const tableHead = ['id', 'name', 'status'];
+
+
+class Table extends React.Component {
+    render() {
+        return (
             <Table>
+
+                <TableHeader>
+                    {
+                        _.map(tableHead, item => {
+                            <TableHeaderColumn>
+                                {item}
+                            </TableHeaderColumn>
+                        })
+                    }
+                </TableHeader>
+                <TableBody>
+                    {
+                        _.map(tableData, item => {
+                            return (
+                                <TableRow>
+                                    {
+                                        _.map(item, (index, x) => <TableRowColumn>
+                                            {
+                                                x[index]
+                                            }
+                                        </TableRowColumn>)
+                                    }
+                                </TableRow>
+                            )
+                        })
+                    }
+                </TableBody>
 
             </Table>
         )
