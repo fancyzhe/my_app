e565d8e923e5a51a3d41e7bd0e3d060b445bb04b
fanz01 <fanz01@mingyuanyun.com>
2018-01-30 16:45:04 +0800

    '增加table组件'

---
+++ src/common/table.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @fanz
+ */
+
+import React from 'react';
+import {Table} from 'material-ui/Table'
+
+class Table extends React.Component{
+    render(){
+        return(
+            <Table>
+
+            </Table>
+        )
+    }
+}
+
+export default Table
