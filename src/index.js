ea5f09c9f140db050230065288d2f970c288dcc7
fanz01 <fanz01@mingyuanyun.com>
2018-01-30 09:41:11 +0800

    '修改readME文件'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter, Route} from "react-router-dom";
 import Manage from "./public/Manage";
-import {setTitle} from "./common/uitls";
+import {setTitle} from "./common/utils";
 
 ReactDOM.render((
         <BrowserRouter>
