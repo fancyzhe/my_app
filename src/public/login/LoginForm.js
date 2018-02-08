3cb4400a53d36d94b7966a60a9881c47d9431b45
fanz01 <fanz01@mingyuanyun.com>
2018-02-08 16:29:24 +0800

    ''

--- src/public/login/LoginForm.js
+++ src/public/login/LoginForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import R from 'request';
-import {Link,browserHistory} from "react-router";
-import {TextField, RaisedButton} from 'material-ui';
+import {Link, browserHistory} from "react-router";
+import {TextField, RaisedButton, Paper} from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import "./login.css"
 import Sbar from "../../common/Snacbar";
@@ -16,72 +16,68 @@ class LoginForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            id:'',
-            pwd:'',
+            id: '',
+            pwd: '',
             errorIdText: '',
             errorPwdText: '',
-            status:false,
-            pageMsg:false,
+            status: false,
+            pageMsg: false,
         }
     }
 
     check() {
-        const {id, pwd,status} = this.state;
+        const {id, pwd, status} = this.state;
         // const getLogin = ({id}) => R.post('/login_form',)
-
-        pwd.length === 0 ? this.setState({errorPwdText: "密码不能为空"}) : this.setState({errorPwdText: "",status:true});
-        id.length === 0 ? this.setState({errorIdText: "用户名不能为空"}) : this.setState({errorIdText: "",status:true});
-
-        if(status){
-            if( id==='1'&&pwd==='1'){
-                browserHistory.push('/user')
-            }else if(id === '2' &&pwd === '2'){
-                browserHistory.push('/manage')
-            } else {
-                this.setState({
-                    pageMsg:true
-                })
-            }
+        if (id === '1' && pwd === '1') {
+            browserHistory.push('/user')
+        } else if (id === '2' && pwd === '2') {
+            browserHistory.push('/manage')
+        } else {
+            pwd.length === 0 ? this.setState({errorPwdText: "密码不能为空"}) : this.setState({errorPwdText: "", status: true});
+            id.length === 0 ? this.setState({errorIdText: "用户名不能为空"}) : this.setState({errorIdText: "", status: true});
+            status && this.setState({
+                pageMsg: true
+            })
         }
 
     }
 
-    msg(){
-        if(this.state.pageMsg){
-            return(<Sbar text='密码错误！'/>)
+    msg() {
+        if (this.state.pageMsg) {
+            return (<Sbar text='密码错误！'/>)
         }
     }
 
 
-    setPwd(e,pwd){
+    setPwd(e, pwd) {
         this.setState({pwd});
         pwd.length > 8 ? this.setState({errorPwdText: "密码不能超过八位~"}) : this.setState({errorPwdText: ""});
     }
 
-    setId(e,id){
+    setId(e, id) {
         this.setState({id});
         id.length > 8 ? this.setState({errorIdText: "用户名不能超过八位~"}) : this.setState({errorIdText: ""});
     }
 
-    componentDidMount(){
+    componentDidMount() {
     }
 
     render() {
 
-        const {errorIdText,errorPwdText,status} = this.state;
+        const {errorIdText, errorPwdText} = this.state;
 
         return (
             <div>
                 <div className="login">
-                    <form className="form">
-                        <div className="img_login"><img/></div>
-                        <MuiThemeProvider>
+                    <MuiThemeProvider>
+                        <Paper className="form" zDepth={3}>
+                            <div className="img_login"><img/></div>
                             <TextField
                                 hintText="输入你的用户名"
                                 floatingLabelText="用户名"
                                 fullWidth={true}
                                 errorText={errorIdText}
-                                onChange={(e,v) => this.setId(e,v)}
+                                onChange={(e, v) => this.setId(e, v)}
                             />
                             <TextField
                                 hintText="输入你的密码"
@@ -89,22 +85,22 @@ class LoginForm extends React.Component {
                                 fullWidth={true}
                                 type="password"
                                 errorText={errorPwdText}
-                                onChange={(e,v)=> this.setPwd(e,v)}
+                                onChange={(e, v) => this.setPwd(e, v)}
                             />
                             <RaisedButton
-                                    label="登陆"
-                                    primary={true}
-                                    className="btn_login"
-                                    onClick={this.check.bind(this)}
+                                label="登陆"
+                                primary={true}
+                                className="btn_login"
+                                onClick={this.check.bind(this)}
                             />
-                            <Link to={'/Forget'} className = "a_login">
+                            <Link to={'/Forget'} className="a_login">
                                 忘记密码...
                             </Link>
                             {
                                 this.msg()
                             }
-                        </MuiThemeProvider>
-                    </form>
+                        </Paper>
+                    </MuiThemeProvider>
                 </div>
             </div>
         )
